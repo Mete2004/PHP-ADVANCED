d90349521624cd16d7112247e99817400a066304
Mete2004 <2150982@talnet.nl>
2022-02-08 13:26:58 +0100

    2.2

---
+++ 2-Loops/Opdracht-2.2/loops-while.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+echo "<ul>";
+for($loop = 0; $loop < 18; $loop++){
+     echo "<li> ik ben $loop jaar, dus ik heb geen stemrecht. </li>";
+}
+    echo "<li> Ik ben 18 jaar oud dus ik heb stemrecht. </li>";
+echo "</ul>"
+?>
+</body>
+</html>
