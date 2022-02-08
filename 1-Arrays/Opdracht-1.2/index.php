52bfc865fb57c646a0df8caec7690878b9e6fcc6
Mete2004 <2150982@talnet.nl>
2022-02-08 11:57:13 +0100

    1.0

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$naam["voornaam"]   = "Willem";
+$naam["achternaam"] = "Alexander van Oranje";
+$naam["leeftijd"]    = 53;
+$naam["opleiding"]   = "Software ontwikkeling";
+
+
+
+echo  $naam['voornaam']. " " .  $naam['achternaam'] . " is " . $naam['leeftijd'] . "jaar oud en is student aan het ROC van Amsterdam aan de opleiding " . $naam['opleiding'] ;
+?>
