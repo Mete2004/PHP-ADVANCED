bc84180d7ee06f499b1e34affe9cf5a46134663d
Mete2004 <2150982@talnet.nl>
2022-02-14 14:43:13 +0100

    3.1

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,10 @@
+<?php
+echo $_POST["Vnaam"]. "<br>";  
+echo $_POST["Anaam"]. "<br>";  
+echo $_POST["klas"]. "<br>";    
+echo $_POST["Leeftijd"]. "<br>";    
+echo $_POST["Adres"]. "<br>";   
+echo $_POST["Pnaam"]. "<br>";    
+echo $_POST["Fm"]; 
+
+?>
