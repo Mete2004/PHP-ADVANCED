a1178a50e79457fefb18ce200d865bbad4e1c7c4
Mete2004 <2150982@talnet.nl>
2022-02-17 09:30:52 +0100

    4.1

---
+++ 4-Functions/Opdracht-4.1/functies-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function schrijfKleur($colorstyle){
+ echo "<font color='$colorstyle'>Deze tekst heeft de kleur $colorstyle</font>";
+}
+
+schrijfKleur ("Red");
+echo "<br>";
+schrijfKleur ("Green");
+echo "<br>";
+schrijfKleur ("Yellow");
+echo "<br>";
+schrijfKleur ("Blue");
+
+?>
+
+</body>
+</html>
