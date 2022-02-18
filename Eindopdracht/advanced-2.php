b9f5a90552f74d564db1fa26805082ee2beb4ee6
Mete2004 <2150982@talnet.nl>
2022-02-18 10:43:52 +0100

    Eindopdracht

--- Eindopdracht/advanced-2.php
+++ Eindopdracht/advanced-2.php
@@ -1,6 +1,43 @@
 <!DOCTYPE html>
 <html>
 <body>
-    
+<style> 
+    table, tr, th{
+        border: <?php echo $_POST["tabelrand"]?>px black;
+        padding: <?php echo $_POST["celpadding"]?>px;
+        background-color: <?php echo $_POST["achtergrondkleur"]?>;
+        color:  <?php echo $_POST["tekstkleur"]?>;
+    }
+    </style>
+</head>
+
+<table>
+<tr>
+    <th>Key</th>
+    <th>Value</th>
+</tr>
+
+<?php 
+    $student = array("Naam"=>"Mete",
+                     "Klas"=>"LCTSDO1A", 
+                     "Leeftijd"=>"17", 
+                     "Woonplaats"=>"Amsterdam", 
+                     "Muzieksmaak"=>"hiphop", 
+                     "Sport"=>"Zwemmen");
+
+    function maakRij($sleutel, $waarde){ 
+        echo "<tr>";
+        echo "<td>$sleutel</td>";
+        echo "<td>$waarde</td>";
+        echo "</tr>";
+     } 
+
+ foreach($student as $key => $value){
+     maakRij($key, $value);
+}
+?>
+</table>
+</form>
+
 </body>
 </html>
