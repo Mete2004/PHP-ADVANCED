fcaefe595ad6cf21335c45c45990a506ca35b664
Mete2004 <2150982@talnet.nl>
2022-03-01 12:58:05 +0100

    eindopdracht

--- Eindopdracht/advanced-1.php
+++ Eindopdracht/advanced-1.php
@@ -1,34 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M3t3k@@n</title>
+</head>
 <body>
+    
+<form name="Forum" method="POST" action="advanced-2.php">
+<p>Tekstkleur: 
+    <select id="kleur" name="color"></p>
+        <?php 
+        $kleuren = ['red' , 'blue' , 'green' , 'black' , 'brown'];
+        foreach($kleuren as $kleur){
+            echo "<option value='$kleur' selected>$kleur</option>";
+        }
+        ?>
+    </select></p>
 
-<form action="advanced-2.php" method="POST">
-<tabel>
-    Tabelranddikte (px): <input type="text" name="tabelrand" id="tabelrand"><br>
-    Celpadding (px): <input type="text" name="celpadding" id="celpadding"><br>
-    Achtergrondkleur: <select name="achtergrondkleur" id="achtergrondkleur">
-
-    <?php  
-    $kleuren = array("red", "blue", "green", "black", "brown");
-    foreach ($kleuren as $achtergrondkleur){
-        echo "<option value='$achtergrondkleur'>$achtergrondkleur</option>";
-    }
-    ?>
-    </select><br>
-    <label for="tekstkleur">Tekstkleur:</label>
-    <select name="tekstkleur" id="tekstkleur">
-    <?php
-    foreach ($kleuren as $value){
-        echo "<option value='$value'>$value</option>";
-    } 
-    ?>
-    </select>
-    <br>
-
-<input type="submit" name="submit" value="verstuur"><br>
-
-
+<p>Achtergrondkleur:
+    <select id="bkg-color" name="backgroundColor"></p>
+    <?php 
+        $achtergrondkleuren = ['red' , 'blue' , 'green' , 'black' , 'brown'];
+        foreach($achtergrondkleuren as $achtergrondkleur){
+            echo "<option value='$achtergrondkleur' selected>$achtergrondkleur</option>";
+        }
+        ?>
+    </select></p>
+<p>Naam: <input name="name"></p>
+<p>Leeftijd: <input name="age"></p>
+<p>Lengte: <input name="height"></p>
+<p>Sport: <input name="sport"></p>
+<p>Hobby's: <input name="hobbies"></p>
+<p>Tabel-border dikte: <input name="border-dikte"></p>
+<p>padding: <input name="padding"></p>
+<button>Verstuur</button>
 </form>
-</tabel>
+
 </body>
 </html>
--- Eindopdracht/advanced-2.php
+++ Eindopdracht/advanced-2.php
@@ -1,43 +1,40 @@
 <!DOCTYPE html>
-<html>
-<body>
-<style> 
-    table, tr, th{
-        border: <?php echo $_POST["tabelrand"]?>px black;
-        padding: <?php echo $_POST["celpadding"]?>px;
-        background-color: <?php echo $_POST["achtergrondkleur"]?>;
-        color:  <?php echo $_POST["tekstkleur"]?>;
-    }
-    </style>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
 </head>
-
-<table>
-<tr>
-    <th>Key</th>
-    <th>Value</th>
-</tr>
-
+<body>
+    
 <?php 
-    $student = array("Naam"=>"Mete",
-                     "Klas"=>"LCTSDO1A", 
-                     "Leeftijd"=>"17", 
-                     "Woonplaats"=>"Amsterdam", 
-                     "Muzieksmaak"=>"hiphop", 
-                     "Sport"=>"Zwemmen");
-
-    function maakRij($sleutel, $waarde){ 
-        echo "<tr>";
-        echo "<td>$sleutel</td>";
-        echo "<td>$waarde</td>";
-        echo "</tr>";
-     } 
-
- foreach($student as $key => $value){
-     maakRij($key, $value);
-}
+echo "<body style='background-color:".$_POST['backgroundColor']."'>";
 ?>
-</table>
-</form>
+
+<?php
+    function maakRij($ranja, $GT){
+        echo "<tr>";
+        echo "<td style='border: ".$_POST['border-dikte']."px solid; color:".$_POST['color']."; padding:".$_POST['padding']."px;'>$ranja</td>";
+        echo "<td style='border: ".$_POST['border-dikte']."px solid; color:".$_POST['color']."; padding:".$_POST['padding']."px;'>$GT</td>";
+        echo "</tr>";
+    }
+
+    $mezelf = [
+    'naam'     => $_POST['name'],
+    'leeftijd' => $_POST['age'],
+    'lengte'   => $_POST['height'],
+    'sport'    => $_POST['sport'],
+    'hobbies'  => $_POST['hobbies'],
+    ]; 
+
+    echo "<table style='border:  ".$_POST['border-dikte']."px solid' width='300' cellspacing='0'>";
+
+    foreach($mezelf as $key =>$me){
+         maakRij($key, $me);
+    }
+    echo "</table>"
+?>
 
 </body>
 </html>
